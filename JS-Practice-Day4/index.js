8708b1728e5fd832ae9d55632c95977963354ecd
1234harshada <harshadarundhey@gmail.com>
2025-06-28 03:53:05 +0530

    JS-Day4

---
+++ JS-Practice-Day4/index.js
@@ -0,0 +1,13 @@
+// For Loop: 1 to 5
+console.log("For Loop (1 to 5):");
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+// While Loop: 5 to 1
+console.log("While Loop (5 to 1):");
+let i = 5;
+while (i >= 1) {
+  console.log(i);
+  i--; 
+}
